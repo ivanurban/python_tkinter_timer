7ecefda69b3eb162fbc3d61e904b5b986b4c6ab3
Ivan Urban <ivanurban_bg@yahoo.com>
2020-07-10 16:19:31 +0200

    Added topmost

--- timer.py
+++ timer.py
@@ -8,12 +8,12 @@ import tkinter as tk
 
 
 def timer(time_to_sleep, title, text_message):
+
     root=tk.Tk()
 
     time.sleep(time_to_sleep)
 
     root.title(title)
-
 
     label= tk.Label(root, text=text_message)
     label.pack(side="top", fill="both", expand="True", padx=20, pady=20)
@@ -21,12 +21,15 @@ def timer(time_to_sleep, title, text_message):
     button = tk.Button(root, text="OK", command=lambda: root.destroy())
     button.pack(side="bottom", fill="none", expand="True")
 
+    
+    #keep the popup above all other opened windows
+    root.wm_attributes("-topmost", 1)
 
     root.mainloop()
 
 
     
-
+#Call function, pass the time and text that you want
 timer(2700, "Pause", "TIME TO MAKE PAUSE")
 
 timer(900, "Work", "BACK TO WORK")
