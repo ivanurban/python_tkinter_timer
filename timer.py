dec0a7ac165f6ec70b1243bc0c9e7d33c8d65e0a
Ivan Urban <ivanurban_bg@yahoo.com>
2020-07-12 12:29:04 +0200

    Added stop button

--- timer.py
+++ timer.py
@@ -3,6 +3,7 @@
 
 import time
 import tkinter as tk
+import sys
 
 
 
@@ -19,7 +20,10 @@ def timer(time_to_sleep, title, text_message):
     label.pack(side="top", fill="both", expand="True", padx=20, pady=20)
 
     button = tk.Button(root, text="OK", command=lambda: root.destroy())
-    button.pack(side="bottom", fill="none", expand="True")
+    button.pack(side="left", fill="none", expand="True")
+    
+    button = tk.Button(root, text="Stop", command=lambda: sys.exit())
+    button.pack(side="right", fill="none", expand="True")
 
     
     #keep the popup above all other opened windows
@@ -28,8 +32,8 @@ def timer(time_to_sleep, title, text_message):
     root.mainloop()
 
 
-    
-#Call function, pass the time and text that you want
-timer(2700, "Pause", "TIME TO MAKE PAUSE")
+while True:
+    #Call function, pass the time and text that you want
+    timer(5, "Pause", "TIME TO MAKE PAUSE")
 
-timer(900, "Work", "BACK TO WORK")
+    timer(2, "Work", "BACK TO WORK")
